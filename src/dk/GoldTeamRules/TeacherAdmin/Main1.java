841f904871bb0c8825b02e14587040f04a3be6d2
Kathurjan <kath097c@easv365.dk>
2022-02-21 17:27:25 +0100

    might need to disgard

--- src/dk/GoldTeamRules/TeacherAdmin/Main1.java
+++ src/dk/GoldTeamRules/TeacherAdmin/Main1.java
@@ -6,9 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.awt.*;
-
-public class Main extends Application {
+public class Main1 extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/dk/GoldTeamRules/TeacherAdmin/GUI/View/Adminlogin.fxml"));
